18baf4038e803a7ca75865e47b78e066f2a48ded
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-08-02 22:55:46 +0300

    checking skill

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class CommandLine {
     static String arr[] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
     public static void main(String args[]){
+        int counter = 0;
+        while (counter < 10) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter your Number : ");
+            int containt = input.nextInt();
+            System.out.println(arr[containt]);
 
-        for(int i = 0;i< args.length;i++)
-            System.out.println("arr["+ i +  "] : "+ args[i]);
+            counter++;
+        }
+
+
     }
 }

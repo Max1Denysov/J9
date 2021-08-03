a9927dae4b95e0af35967c687b7313acbac49560
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-08-03 17:27:20 +0300

    var arg array method

--- src/PassArray.java
+++ src/PassArray.java
@@ -1,16 +1,13 @@
 public class PassArray {
-    static void test(int v[]){
+    static void test(int...v){
         System.out.println("Number of argumunts: " + v.length + " Containing :");
         for(int x : v)
         System.out.println(x +"");
         System.out.println();
     }
     public static void main(String args[]){
-        int n1[] = {10};
-        int n2[] = {1,2,3};
-        int n3[] = {};
-        test(n1);
-        test(n2);
-        test(n3);
+        test(10);
+        test(1,2,3);
+        test();
     }
 }

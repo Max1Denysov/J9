e5f389863347678e2befd337e4818759d458ea40
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-08-03 17:56:51 +0300

    reload vararg

--- src/PassArray.java
+++ src/PassArray.java
@@ -1,13 +1,28 @@
 public class PassArray {
-    static void test(String msg, int...v){
-        System.out.print(msg + v.length + " Containing : ");
+    static void test( int...v){
+        System.out.print("Test(int...)  -  Numbers of argument: [" + v.length + "] Containing: ");
         for(int x : v)
-            System.out.print(x +" ");
+            System.out.print(x + " ");
         System.out.println();
     }
+
+    static void test(Boolean...v){
+        System.out.print("\nTest(boolean...)  -  Numbers of argument: [" + v.length + "] Containing: ");
+        for(boolean x : v)
+            System.out.print(x + " ");
+        System.out.println();
+
+
+    }
+    static void test(String msg,int...v){
+        System.out.print("Test(String,int...)  " + msg + v.length + "] Containing: ");
+        for(int x : v)
+            System.out.print(x + " ");
+    }
+
     public static void main(String args[]){
-        test("One parameter ",10);
-        test("Two parametres ",2,3);
-        test("Only message");
+        test(10);
+        test("- Numbers of argument: [",2,3);
+        test(true,false,false,true);
     }
 }

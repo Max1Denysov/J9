3d47b575df73f0422f354da573a996d740a60e18
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-08-09 03:07:45 +0300

    update0908

---
+++ src/Terminal.java
@@ -0,0 +1,7 @@
+public class Terminal {
+    public static void main(String args[]){
+        for(int i = 0; i < args.length; i++)
+            System.out.println("Arg [" + i + "] contain:"+ args[i]);
+    }
+
+}
